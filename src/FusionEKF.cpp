57cddcca34b9e48e61f1a0e26271c86c985fe0ac
jalajbidwai <jalajbidwai@gmail.com>
2018-12-20 18:48:01 -0500

    Kalman Filter

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -20,24 +20,42 @@ FusionEKF::FusionEKF() {
   R_laser_ = MatrixXd(2, 2);
   R_radar_ = MatrixXd(3, 3);
   H_laser_ = MatrixXd(2, 4);
-  Hj_ = MatrixXd(3, 4);
+  Hj_      = MatrixXd(3, 4);
 
   //measurement covariance matrix - laser
   R_laser_ << 0.0225, 0,
-        0, 0.0225;
+              0, 0.0225;
 
   //measurement covariance matrix - radar
   R_radar_ << 0.09, 0, 0,
-        0, 0.0009, 0,
-        0, 0, 0.09;
+              0, 0.0009, 0,
+              0, 0, 0.09;
 
   /**
   TODO:
     * Finish initializing the FusionEKF.
     * Set the process and measurement noises
   */
+  H_laser_ << 1, 0, 0, 0,
+              0, 1, 0, 0;
 
+  Hj_ << 1, 1, 0, 0,
+         1, 1, 0, 0,
+         1, 1, 1, 1; 
 
+  // State transition matrix F_ initialization.
+  ekf_.F_ = MatrixXd(4, 4);
+  ekf_.F_ << 1, 0, 1, 0,
+             0, 1, 0, 1,
+             0, 0, 1, 0,
+             0, 0, 0, 1;
+
+  //state covariance matrix P
+  ekf_.P_ = MatrixXd(4, 4);
+  ekf_.P_ << 1, 0, 0, 0,
+             0, 1, 0, 0,
+             0, 0, 1000, 0,
+             0, 0, 0, 1000;
 }
 
 /**
@@ -63,16 +81,26 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     ekf_.x_ = VectorXd(4);
     ekf_.x_ << 1, 1, 1, 1;
 
-    if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
+    if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
       /**
-      Convert radar from polar to cartesian coordinates and initialize state.
+       Initialize state.
       */
+	  ekf_.x_ << measurement_pack.raw_measurements_(0),measurement_pack.raw_measurements_(1),0,0;
+      
     }
-    else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
+    else if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
       /**
-      Initialize state.
+       Convert radar from polar to cartesian coordinates and initialize state.
       */
+      float rho     = measurement_pack.raw_measurements_(0);
+      float phi     = measurement_pack.raw_measurements_(1);
+      float rho_dot = measurement_pack.raw_measurements_(2);
+	  
+     	  
+	  ekf_.x_<<rho*cos(phi),rho*sin(phi),rho_dot*cos(phi),rho_dot*sin(phi);
     }
+
+    previous_timestamp_ = measurement_pack.timestamp_;
 
     // done initializing, no need to predict or update
     is_initialized_ = true;
@@ -91,6 +119,30 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
      * Use noise_ax = 9 and noise_ay = 9 for your Q matrix.
    */
 
+  //Delta time between the current and previous time stamps.
+  float dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0;	//dt - expressed in seconds
+  previous_timestamp_ = measurement_pack.timestamp_;
+
+  
+  float tmp2 = dt*dt;
+  float tmp3 = (tmp2*dt)/2;
+  float tmp4 = (tmp3*dt)/4;
+
+  //State Transition Matrix.
+  ekf_.F_(0, 2) = dt;
+  ekf_.F_(1, 3) = dt;
+
+  //Noise
+  float noise_ax = 9;
+  float noise_ay = 9;
+
+  // Process Covariance Matrix.
+  ekf_.Q_ = MatrixXd(4, 4);
+  ekf_.Q_ << tmp4 * noise_ax, 0, tmp3 * noise_ax, 0,
+             0, tmp4 * noise_ay, 0, tmp3 * noise_ay,
+             tmp3 * noise_ax, 0, tmp2*noise_ax, 0,
+             0, tmp3 * noise_ay, 0, tmp2*noise_ay;
+
   ekf_.Predict();
 
   /*****************************************************************************
@@ -105,8 +157,16 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // Radar updates
+    Tools tools;
+    Hj_ = tools.CalculateJacobian(ekf_.x_);
+    ekf_.H_ = Hj_;
+    ekf_.R_ = R_radar_;
+    ekf_.UpdateEKF(measurement_pack.raw_measurements_);
   } else {
     // Laser updates
+    ekf_.H_ = H_laser_;
+    ekf_.R_ = R_laser_;
+    ekf_.Update(measurement_pack.raw_measurements_);
   }
 
   // print the output
--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -3,9 +3,6 @@
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
-// Please note that the Eigen library does not initialize 
-// VectorXd or MatrixXd objects with zeros upon creation.
-
 KalmanFilter::KalmanFilter() {}
 
 KalmanFilter::~KalmanFilter() {}
@@ -22,21 +19,77 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
 
 void KalmanFilter::Predict() {
   /**
-  TODO:
+  DONE:
     * predict the state
   */
+  x_ = F_ * x_;
+  P_ = F_ * P_ * F_.transpose() + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
   /**
-  TODO:
+  DONE:
     * update the state by using Kalman Filter equations
   */
+  MatrixXd Ht = H_.transpose();
+
+  VectorXd y = z - (H_ * x_);
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd K = P_ * Ht * S.inverse();
+
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
   /**
-  TODO:
+  DONE:
     * update the state by using Extended Kalman Filter equations
   */
+
+  // Define local variables to work on
+  float px = x_[0];
+  float py = x_[1];
+  float vx = x_[2];
+  float vy = x_[3];
+  
+  float rho = sqrt(pow(px,2)+pow(py,2));
+  float phi = atan2(py,px); 
+  float rho_dot;
+
+  //Checking division by zero.
+  if (fabs(rho) < 0.0001) {
+    rho_dot = 0;
+  } else {
+    rho_dot = (vx*px + vy*py)/rho;
+  }
+
+  // Vector containing predicted values.
+  VectorXd z_pred (3);
+  z_pred << rho, phi, rho_dot;
+
+  // Difference between actual radar values and prediction.
+  VectorXd y = z - z_pred;
+
+  //Ensuring that the phi value always stays between -Pi and +Pi.
+  while (y[1] > M_PI){
+     y[1] -= (2 * M_PI);
+  }
+  while (y[1] < - M_PI){
+     y[1] += (2 * M_PI);
+  }
+
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
